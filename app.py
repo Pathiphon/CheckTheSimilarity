18bc9e5af8cf4dd25f7d3304c2cdf7efb9b83761
Pathiphon <75231048+Pathiphon@users.noreply.github.com>
2022-02-26 10:26:59 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from pythainlp.word_vector import * # ทำการเรียก thai2vec
 from sklearn.metrics.pairwise import cosine_similarity  # ใช้หาค่าความคล้ายคลึง
 import numpy as np
 from flask import Flask, request
+from flask_cors import CORS
 import json 
    
 app = Flask(__name__) 
+CORS(app)
 
 
 @app.route('/checksimilarity', methods = ['POST']) 
